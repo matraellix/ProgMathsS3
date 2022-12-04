020f0c54fd932d77b638a71c9aba61a54b1ec504
matraellix <maelle.fleitz@gmail.com>
2022-12-04 09:07:17 +0100

    create Cmake

---
+++ build/my_examples/CMakeFiles/My_Examples.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "../bin/My_Examples"
+  "../bin/My_Examples.pdb"
+  "CMakeFiles/My_Examples.dir/src/main.cpp.o"
+  "CMakeFiles/My_Examples.dir/src/test1.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/My_Examples.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
